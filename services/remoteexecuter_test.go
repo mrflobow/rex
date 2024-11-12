3e903fbbbf6fc8e72182a8bfd60216c872480a86
mrflobow <30889461+mrflobow@users.noreply.github.com>
2024-11-12 20:05:24 +0100

    Added single command test

--- services/remoteexecuter_test.go
+++ services/remoteexecuter_test.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"log"
 	"testing"
 
 	"github.com/mrflobow/rex/models"
@@ -20,4 +21,27 @@ func TestConnectionFailed(t *testing.T) {
 		t.Fatal("Expected connection error")
 	}
 
+}
+
+func TestSingleCommandExec(t *testing.T) {
+	var load ConfigLoader
+	config, err := load.LoadConfig("../testdata/config.yml")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rex := RemoteExecutor{Config: config}
+	args := []string{"echo \"hello world\""}
+
+	out, err := rex.ExecuteCommand("test1", args)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sout := string(out.Data)
+	if sout != "hello world\n" {
+		log.Fatalf("Result mismatches %v , expected: hello world", sout)
+	}
 }
